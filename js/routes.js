0b05d9c04a33de87aff10fa7ee4563e44ea6bf4d
matt <matt.flesher@gmail.com>
2013-08-09 11:16:54 -0600

    routes

---
+++ js/routes.js
@@ -0,0 +1,44 @@
+/***********************************
+ * Router
+ *
+ * routes for project details
+************************************
+/                                  */
+
+(function () {
+
+  var AppRouter = Backbone.Router.extend({
+    routes: {
+      "wait": "waitingMusic", 
+      "time": "deliveryTime", 
+      "*actions": "defaultRoute"
+      }
+  });
+
+  var app_router = new AppRouter;
+
+  app_router.on('route:waitingMusic', function() {
+    $.get("partials/waitingmusic.html", function(data){
+      console.log(data);
+      $('#the-view').html(data);     
+    });
+  });
+
+  app_router.on('route:deliveryTime', function() {
+    $.get("partials/deliverytime.html", function(data){
+      console.log(data);
+      $('#the-view').html(data);     
+    });
+  });
+
+  app_router.on('route:defaultRoute', function() {
+    $.get("partials/location.html", function(data){
+      console.log(data);
+      $('#the-view').html(data);     
+    });
+
+  });
+
+  Backbone.history.start();
+
+})();
